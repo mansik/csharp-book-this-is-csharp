d0a2711d53686ad6fb07f516a90eb75a2342b20f
Mansik Kim <k.mansik@gmail.com>
2023-02-22 04:18:21 +0900

    add 15

---
+++ 15장/ListSort/MainApp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ListSort
+{
+    class Profile
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Profile[] arrProfile = {
+                //new Profile() {Name = "이문세", Height = 178}, // ()는 생략가능
+                // 개체 이니셜라이저를 이용한 초기화
+                new Profile {Name = "정우성", Height = 186}, 
+                new Profile {Name = "김태희", Height = 158},
+                new Profile {Name = "고현정", Height = 172},
+                new Profile {Name = "이문세", Height = 178},
+                new Profile {Name = "하동훈", Height = 171}
+            };
+
+
+            #region 1. Linq를 이용하지 않는 Sort
+            // arrProfile 안에 있는 각 데이터로부터 Height가 175 미만인 객체만 골라 profiles에 넣은 후            
+            List<Profile> profiles = new List<Profile>();
+            foreach (Profile profile in arrProfile)
+            {
+                if (profile.Height < 175)
+                    profiles.Add(profile);
+            }
+
+            // 키의 오름차순으로 정렬
+            // List<T>.Sort(Comparison<T>) 
+            profiles.Sort(
+                (profile1, profile2) =>
+                {
+                    return profile1.Height - profile2.Height;
+                });
+
+            foreach (var profile in profiles)
+                Console.WriteLine($"{profile.Name}, {profile.Height}");
+            #endregion
+
+
+
+
+            #region 2. Linq를 이용한 Sort
+            
+            var linqProfiles = from profile in arrProfile
+                               where profile.Height < 175
+                               orderby profile.Height
+                               select profile;
+
+            foreach (var profile in linqProfiles)
+                Console.WriteLine($"{profile.Name}, {profile.Height}, Linq");
+
+            #endregion
+
+        }
+
+
+    }
+}
---
+++ 15장/MinMaxAvg/MainApp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http.Headers;
+
+namespace MinMaxAvg
+{
+    class Profile
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Profile[] arrProfile =
+            {
+                new Profile { Name = "정우성", Height = 186 },
+                new Profile { Name = "김태희", Height = 158 },
+                new Profile { Name = "고현경", Height = 172 },
+                new Profile { Name = "이문세", Height = 178 },
+                new Profile { Name = "하하", Height = 171 }
+            };
+
+            // Linq와 표준 연산자(.Max(), .Min(), .Average()) 같이 사용함.
+            var heightStat = from profile in arrProfile
+                             group profile by profile.Height < 175 into g
+                             select new
+                             {
+                                 Group = g.Key == true ? "175미만" : "175이상",
+                                 Count = g.Count(), // 표준 연산자
+                                 Max = g.Max(profile => profile.Height), // 표준 연산자 .Max()
+                                 Min = g.Min(profile => profile.Height), // 표준 연산자
+                                 Average = g.Average(profile => profile.Height) // 표준 연산자
+                             };
+
+            foreach (var stat in heightStat)
+            {
+                Console.Write($"{stat.Group} - Count:{stat.Count}, Max:{stat.Max}, ");
+                Console.WriteLine($"Min:{stat.Min}, Average:{stat.Average}");
+            }
+        }
+    }
+}
