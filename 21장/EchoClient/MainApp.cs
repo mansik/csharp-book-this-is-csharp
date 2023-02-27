e9fd63fa776406061a8cf28d8fc30d6c0c5f9a66
Mansik Kim <k.mansik@gmail.com>
2023-02-28 01:11:48 +0900

    add 21

---
+++ 21장/EchoClient/MainApp.cs
@@ -0,0 +1,72 @@
+﻿
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoClient
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 4)
+            {
+                Console.WriteLine(
+                    "Usage : {0} <Bind IP> <Bind Port> <Server IP> <Message>",
+                    Process.GetCurrentProcess().ProcessName);
+                return;
+            }
+
+            string bindIp = args[0];
+            int bindPort = Convert.ToInt32(args[1]);
+            string serverIp = args[2];
+            const int serverPort = 5425;
+            string message = args[3];
+
+            try
+            {
+                IPEndPoint clientAddress = new IPEndPoint(IPAddress.Parse(bindIp), bindPort);
+                IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+
+                Console.WriteLine("Client : {0}, Server : {1}",
+                    clientAddress.ToString(), serverAddress.ToString());
+
+                // 로컬 TcpClient 객체 생성
+                TcpClient client = new TcpClient(clientAddress);
+
+                // 서버 접속
+                client.Connect(serverAddress);
+
+                byte[] data = Encoding.Default.GetBytes(message);
+
+                // NetworkStream 객체 생성
+                NetworkStream stream = client.GetStream();
+
+                // NetworkStream을 통해 서버에 메시지 보내기
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent : {0}", message);
+
+                // 메시지 받기
+                data = new byte[256];
+                string responseData = "";
+
+                // while ((bytes = stream.Read(data, 0, data.Length)) != 0 ) 이 더 안전하다.
+                int bytes = stream.Read(data, 0, data.Length);
+                responseData = Encoding.Default.GetString(data);
+                Console.WriteLine("Received : {0}", responseData);
+
+                // Close
+                stream.Close();
+                client.Close();
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("Closing Client.");
+        }
+    }
+}
---
+++ 21장/EchoServer/MainApp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoServer
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage : {0} <Bind IP>", Process.GetCurrentProcess().ProcessName);
+                return;
+            }
+
+            string bindIp = args[0];
+            const int bindPort = 5425;
+            TcpListener? server = null;
+            try
+            {
+                IPEndPoint localAddress = new IPEndPoint(IPAddress.Parse(bindIp), bindPort);
+
+                server = new TcpListener(localAddress);
+
+                server.Start();
+
+                Console.WriteLine("Start Echo Server...");
+
+                while (true)
+                {
+                    // AcceptTcpClient()를 호출하면 코드는 블록되어 그 자리에서 이 메소드가 반환할 때까지 진행하지 않는다.
+                    // 클라이언트의 연결 요청이 있기 전까지는 반환되지 않는다.
+                    // 클라이언트의 요청이 오면 TcpClient 객체를 반환한다.
+                    TcpClient client = server.AcceptTcpClient();
+
+                    Console.WriteLine("Connect Client : {0}",
+                        ((IPEndPoint)client.Client.RemoteEndPoint).ToString());
+
+                    // TcpClient 형식의 객체로부터 NetworkStream 형식의 객체를 가져와서 데이터를 읽고 쓸 수 있다.
+                    NetworkStream stream = client.GetStream();
+
+                    int length;
+                    string? data = null;
+                    byte[] bytes = new byte[256];
+
+                    while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = Encoding.Default.GetString(bytes, 0, length);
+                        Console.WriteLine($"Received : {data}");
+
+                        // 클라이언트로 받은 데이터 전송
+                        byte[] msg = Encoding.Default.GetBytes(data);
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine($"Sent : {data}");
+                    }
+
+                    stream.Close();
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                if(server != null) 
+                    server.Stop();
+            }
+
+            Console.WriteLine("Closing Server.");
+        }
+    }
+}
